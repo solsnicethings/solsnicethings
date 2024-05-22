a96b5dc1925d901439b89aca9f43d7a920466289
solsnicethings <aron.riktor@gmail.com>
2024-05-22 10:30:02 +0200

    cite

--- components/byfilename/index/Tip of the day.js
+++ components/byfilename/index/Tip of the day.js
@@ -4,15 +4,23 @@
 	dom = component_registry[dom].dom;
 	
 	dom = dom.appendChild(document.createElement('b'));
-	dom.style.whiteSpace = 'pre';
 
-	let tips = [
-		"tipByDay[Math.floor(new Date().getTime()/8.64e7) % tips.length]"
+	let tip = [
+		{"tipByDay[Math.floor(new Date().getTime()/8.64e7) % tips.length]":
+			'https://stackoverflow.com/questions/12739171/javascript-epoch-time-in-days'
+		},
+		{"For application/x-www-form-urlencoded, spaces are to be replaced by +, so one may wish to follow a encodeURIComponent() replacement with an additional replacement of %20 with +.":
+			'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent'
+		}
 	];
 
-	let tip = Math.floor(new Date().getTime()/8.64e7) % tips.length;
+	tip = tip[Math.floor(new Date().getTime()/8.64e7) % tip.length];
 
-	dom.innerText = tips[tip];
-
+	for (const k in tip) {
+		let e = dom.appendChild( document.createElement('p') );
+		e.style.whiteSpace = 'pre';
+		e.innerText = k;
+		dom.appendChild('cite').innerText = tip[k];
+	}
 		
 })());
