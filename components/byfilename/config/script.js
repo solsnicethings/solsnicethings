2bf2257427642861956056215d810a8a4a515c0c
solsnicethings <aron.riktor@gmail.com>
2024-05-23 22:55:31 +0200

    dont do the dumb thing

--- components/byfilename/config/script.js
+++ components/byfilename/config/script.js
@@ -1,8 +1,7 @@
 ((()=> {
 	let scope = urlSearchParams.get('scope');
 	if (scope) {
-		document.body.classList.add('loadmsg');
-		document.body.setAttribute('first', "Configuring scoped to page: scope");
+		document.body.setAttribute('elemtitle', "Configuring scoped to page: scope");
 	}
 	
 })());
