4c120c108806b3eed5378b6e2285626f257984aa
solsnicethings <aron.riktor@gmail.com>
2024-05-23 22:49:18 +0200

    Update script.js

--- components/byfilename/config/script.js
+++ components/byfilename/config/script.js
@@ -1,8 +1,8 @@
 ((()=> {
 	let scope = urlSearchParams.get('scope');
 	if (scope) {
-		body.classList.add('loadmsg');
-		body.setAttribute('first', "Configuring scoped to page: scope");
+		document.body.classList.add('loadmsg');
+		document.body.setAttribute('first', "Configuring scoped to page: scope");
 	}
 	
 })());
