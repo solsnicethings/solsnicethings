49b96be2c830fcac6c8c1f84b3cfb18f2c0974ac
solsnicethings <aron.riktor@gmail.com>
2024-05-26 21:12:27 +0200

    Update script.js

--- components/byfilename/Configuration/script.js
+++ components/byfilename/Configuration/script.js
@@ -3,7 +3,7 @@ var cfgscope = urlSearchParams.get('scope');
 if (cfgscope) {
 	urlSearchParams.delete('scope');
 	document.body.setAttribute('elemtitle', "You are viewing and changing settings for the page: " + cfgscope);
-	var ga = document.createElement('a');
+	let ga = document.createElement('a');
 	a.setAttribute('href', '?' + urlSearchParams);
 	a.innerText = 'Global Config';
 	a.style.float = 'right';
