c6708f37a60226d0878072bf2d6af1977dace584
solsnicethings <aron.riktor@gmail.com>
2024-06-23 18:00:55 +0200

    Update afterbuild.js

--- afterbuild.js
+++ afterbuild.js
@@ -10,19 +10,25 @@ function mkdir(path, options = { recursive: true }) {
 	if (!fs.statSync(path, {throwIfNoEntry: false})?.isDirectory()) fs.mkdirSync(path, options);
 }
 
+const filesToMake {};
+
 function makesitemap(srcpath = root, bldpath = indexroot, exclusions = [ path.basename(scriptfile) ]) {
 	mkdir(bldpath);
 	const index = [];
 	for (const e of fs.readdirSync(srcpath)) {
 		if (exclusions && exclusions.indexOf(e) >= 0) continue;
-		if (fs.statSync(s).isDirectory()) {
+		if (fs.statSync(path.join(srcpath, e)).isDirectory()) {
 			index.push({ Directory: e });
 			makesitemap(path.join(srcpath, e), path.join(bldpath, e), null, e);
 		}
 		else index.push(e);
 	}
-	bldpath = path.join(bldpath, '_.json');
-	fs.writeFileSync(bldpath, JSON.stringify(index));
+	filesToMake[bldPath] = index;
 }
 
 makesitemap();
+
+for (const f in filesToMake) {
+	mkdir(f);
+	fs.writeFileSync(path.join(f, '_.json'), JSON.stringify(index));
+}
